3793457dccde0c2d09f86bbabf5c76f9f56a6a1e
LiorDaSelior <liorbelenkov@mail.tau.ac.il>
2025-03-19 18:39:25 +0200

    remove test hibernate config

--- src/test/java/com/hinamlist/hinam_list/common/ProducerLevelPropertiesConfiguration.java
+++ src/test/java/com/hinamlist/hinam_list/common/ProducerLevelPropertiesConfiguration.java
@@ -1,9 +1,16 @@
 package com.hinamlist.hinam_list.common;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @TestConfiguration
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 @ComponentScan(basePackages = {"com.hinamlist.hinam_list.service.json_producer",
         "com.hinamlist.hinam_list.service.json_scraper",
         "com.hinamlist.hinam_list.config"
